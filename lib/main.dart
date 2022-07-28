a770b7f494dc52d71e07c0a7cac2eb727b0a55b4
hanuuuuU <han980004@naver.com>
2022-07-28 11:29:41 +0900

    PocketSCH

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: Splash(),
       getPages: [
         //다른 페이지로 파라미터를 간단하게 넘길 때 사용
+        //이름 뒤에 /:사용할 파라미터명 적으면 됨
         GetPage(name: '/notify', page: ()=> NotifyHome()),
         GetPage(name: '/bus', page: ()=> BusHome()),
-        GetPage(name: '/eat', page: ()=> EatHome()),
+        GetPage(name: '/eat', page: ()=> EatHome())
       ],
     );
   }
