54024ca8cde2349510feffd224ec66659b155322
petcu1004 <kty4585@naver.com>
2022-08-17 21:53:54 +0900

    실시간 시간 중단

--- lib/view/bus/bus_home.dart
+++ lib/view/bus/bus_home.dart
@@ -45,12 +45,11 @@ class _BusHomeState extends State<BusHome> {
     SchoolBusGetRequest(changed_day);
 
     ImageGetRequest();
-    // RefreshTime();
   }
 
   void dispose() {
     super.dispose();
-    RefreshTime();
+    // RefreshTime();
   }
 
   String getCurrentDay() {
@@ -60,70 +59,68 @@ class _BusHomeState extends State<BusHome> {
     return _currentDay;
   }
 
-  String RefreshTime() {
-    var time_list = [];
-    time_list.add(0);
-    int min;
-    var initM;
+  // String RefreshTime() {
+  //   var time_list = [];
+  //   time_list.add(0);
+  //   int min;
+  //   var initM;
 
-    double h, m;
-    int h1, m1;
-    double tmp;
-    Timer _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        for (int i = 0; i < this._datas.length; i++) {
-          String time = this._datas[i].busTime;
+  //   double h, m;
+  //   int h1, m1;
+  //   double tmp;
+  //   Timer _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+  //     setState(() {
+  //       for (int i = 0; i < this._datas.length; i++) {
+  //         String time = this._datas[i].busTime;
 
-          // String time_list=this._datas[0]
-          final splitted = time.split('T');
-          // print("학내순환 시간 : " + splitted[1]); //학내순환이니까 10분 간격으로 나옴
-          DateTime formattedTime2 = DateFormat("hh:mm").parse(splitted[1]);
+  //         // String time_list=this._datas[0]
+  //         final splitted = time.split('T');
+  //         // print("학내순환 시간 : " + splitted[1]); //학내순환이니까 10분 간격으로 나옴
+  //         DateTime formattedTime2 = DateFormat("hh:mm").parse(splitted[1]);
 
-          DateTime now = DateTime.now();
-          String formattedTime = DateFormat('kk:mm').format(now);
-          // print("현재시간 : " + formattedTime);
-          DateTime formattedTime1 = DateFormat("hh:mm").parse(formattedTime);
-          // print("formateedTime: " + formattedTime);
+  //         DateTime now = DateTime.now();
+  //         String formattedTime = DateFormat('kk:mm').format(now);
+  //         // print("현재시간 : " + formattedTime);
+  //         DateTime formattedTime1 = DateFormat("hh:mm").parse(formattedTime);
+  //         // print("formateedTime: " + formattedTime);
 
-          Duration duration = formattedTime2.difference(formattedTime1);
-          // print(duration.inSeconds); //계산해서 나온 초
+  //         Duration duration = formattedTime2.difference(formattedTime1);
+  //         // print(duration.inSeconds); //계산해서 나온 초
 
-          if (duration.inSeconds >= 0) {
-            time_list.add(duration.inSeconds);
-          }
-        }
-        // print(time_list); //모든 시간 넣은 리스트(데이터 정제 이전)
-      });
-    });
+  //         if (duration.inSeconds >= 0) {
+  //           time_list.add(duration.inSeconds);
+  //         }
+  //       }
+  //       // print(time_list); //모든 시간 넣은 리스트(데이터 정제 이전)
+  //     });
+  //   });
 
-    min = time_list[0];
-    // print(min); //가장 얼마 안남은 시간
+  //   min = time_list[0];
+  //   // print(min); //가장 얼마 안남은 시간
 
-    initM = min;
+  //   initM = min;
 
-    // h = initM / 3600;
-    // h1 = h.toInt();
-    m = (initM % 3600) / 60;
-    m1 = m.toInt();
+  //   // h = initM / 3600;
+  //   // h1 = h.toInt();
+  //   m = (initM % 3600) / 60;
+  //   m1 = m.toInt();
 
-    String result = "$m1분 뒤 출발"; //남은 시간
-    return result;
-  }
+  //   String result = "$m1분 뒤 출발"; //남은 시간
+  //   return result;
+  // }
 
 //학내순환 버스 Timer (몇분 남았는지 확인하기위해) - 리스트 보여주는 부분에도 쓰일 함수
   String ChangeTime() {
     // String time =
     //     this._datas[0].busTime; //첫 번쨰 버스 시간을 가져온거임 -> 가까운 시간을 가져올 수 있도록 하기
     var time_list = [];
+    // time_list.add(1);
     int min;
     var initM;
 
     double h, m;
     int h1, m1;
     double tmp;
-    // Timer _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-    //   setState(() {});
-    // });
 
     for (int i = 0; i < this._datas.length; i++) {
       String time = this._datas[i].busTime;
@@ -157,6 +154,13 @@ class _BusHomeState extends State<BusHome> {
     // h1 = h.toInt();
     m = (initM % 3600) / 60;
     m1 = m.toInt();
+
+    // if (time_list[0] == 1) {
+    //   return "버스가 없습니다.";
+    // } else {
+    //   String result = "$m1분 뒤 출발"; //남은 시간
+    //   return result;
+    // }
 
     String result = "$m1분 뒤 출발"; //남은 시간
     return result;
@@ -326,43 +330,43 @@ class _BusHomeState extends State<BusHome> {
                             SizedBox(
                               width: 15,
                             ),
-                            FutureBuilder(builder:
-                                (BuildContext context, AsyncSnapshot snapshot) {
-                              //해당 부분은 data를 아직 받아 오지 못했을때 실행되는 부분을 의미한다.
-                              if (snapshot.hasData == false) {
-                                return IconButton(
-                                  onPressed: RefreshTime,
-                                  icon: Icon(Icons.refresh_outlined),
-                                  iconSize: 30,
-                                );
-                              }
-                              //error가 발생하게 될 경우 반환하게 되는 부분
-                              else if (snapshot.hasError) {
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    'Error: ${snapshot.error}',
-                                    style: TextStyle(fontSize: 15),
-                                  ),
-                                );
-                              }
-                              // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 것이다.
-                              else {
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: IconButton(
-                                    onPressed: RefreshTime,
-                                    icon: Icon(Icons.refresh_outlined),
-                                    iconSize: 30,
-                                  ),
-                                );
-                              }
-                            }),
-                            // IconButton(
-                            //   onPressed: RefreshTime,
-                            //   icon: Icon(Icons.refresh_outlined),
-                            //   iconSize: 30,
-                            // ),
+                            // FutureBuilder(builder:
+                            //     (BuildContext context, AsyncSnapshot snapshot) {
+                            //   //해당 부분은 data를 아직 받아 오지 못했을때 실행되는 부분을 의미한다.
+                            //   if (snapshot.hasData == false) {
+                            //     return IconButton(
+                            //       onPressed: RefreshTime,
+                            //       icon: Icon(Icons.refresh_outlined),
+                            //       iconSize: 30,
+                            //     );
+                            //   }
+                            //   //error가 발생하게 될 경우 반환하게 되는 부분
+                            //   else if (snapshot.hasError) {
+                            //     return Padding(
+                            //       padding: const EdgeInsets.all(8.0),
+                            //       child: Text(
+                            //         'Error: ${snapshot.error}',
+                            //         style: TextStyle(fontSize: 15),
+                            //       ),
+                            //     );
+                            //   }
+                            //   // 데이터를 정상적으로 받아오게 되면 다음 부분을 실행하게 되는 것이다.
+                            //   else {
+                            //     return Padding(
+                            //       padding: const EdgeInsets.all(8.0),
+                            //       child: IconButton(
+                            //         onPressed: RefreshTime,
+                            //         icon: Icon(Icons.refresh_outlined),
+                            //         iconSize: 30,
+                            //       ),
+                            //     );
+                            //   }
+                            // }),
+                            IconButton(
+                              onPressed: ChangeTime,
+                              icon: Icon(Icons.refresh_outlined),
+                              iconSize: 30,
+                            ),
                           ],
                         ),
                         SizedBox(
