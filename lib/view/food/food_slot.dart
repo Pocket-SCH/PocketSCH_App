b6c31b3e7681375079947e10e0c3744c374ebbb0
Choo Chang Woo <cnckddn0146@gmail.com>
2022-08-24 16:50:41 +0900

    슬롯 머신 완성

--- lib/view/food/food_slot.dart
+++ lib/view/food/food_slot.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pocket_sch/controller/solid_button_builder.dart';
@@ -15,163 +18,245 @@ class FoodSlot extends StatefulWidget {
 
 class _FoodSlotState extends State<FoodSlot> {
   late FoodSlotController foodSlotController;
+  late ConfettiController _confettiController;
+
   @override
   void initState() {
     super.initState();
     foodSlotController = FoodSlotController();
+    _confettiController =
+        ConfettiController(duration: const Duration(seconds: 10));
+  }
+
+  @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: CustomColor.background,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: Image.asset('assets/btn_back.png',
-              color: Color.fromARGB(255, 0, 0, 0), width: 15),
-          onPressed: () => Get.off(() => FoodCategory()),
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Center(
-          child: Column(
-            children: [
-              RichText(
-                text: TextSpan(children: const <TextSpan>[
-                  TextSpan(
-                      text: ' RANDOM ',
-                      style: TextStyle(
-                          backgroundColor: CustomColor.primary,
-                          fontSize: 18,
-                          color: Colors.white)),
-                  TextSpan(text: ' '),
-                  TextSpan(
-                      text: '음식',
-                      style: TextStyle(color: Colors.black, fontSize: 18))
-                ]),
-              ),
-              SizedBox(height: 15),
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  child: Card(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6),
-                      side: BorderSide(
-                        color: Colors.grey.withOpacity(0.3),
-                        width: 1,
-                      ),
-                    ),
-                    child: Column(children: [
-                      Flexible(
-                          flex: 3,
-                          child: Container(
-                            height: double.infinity,
-                            color: Colors.black,
-                            child: Stack(
-                              children: [
-                                Container(
-                                  height: double.infinity,
-                                  color: Colors.white,
-                                  child: Image(
-                                      fit: BoxFit.fill,
-                                      image: AssetImage(
-                                          'assets/img_slot_modified.png')),
-                                ),
-                                Column(
-                                  children: [
-                                    Flexible(flex: 150, child: Container()),
-                                    Flexible(
-                                        flex: 130,
-                                        child: Row(
-                                          children: [
-                                            Flexible(
-                                                flex: 110, child: Container()),
-                                            Flexible(
-                                                flex: 300,
-                                                //여기에 룰렛 삽입
-                                                child: Container(
-                                                  decoration: BoxDecoration(
-                                                      border: Border.all(
-                                                          width: 8,
-                                                          color: Colors.black
-                                                              .withOpacity(
-                                                                  0.4)),
-                                                      borderRadius:
-                                                          BorderRadius.all(
-                                                              Radius.circular(
-                                                                  10))),
-                                                  child: Stack(
-                                                    children: [
-                                                      Container(
-                                                        child: FoodSlotMachine(
-                                                            controller:
-                                                                foodSlotController),
-                                                      ),
-                                                      Center(
-                                                        child: Container(
-                                                          width:
-                                                              double.infinity,
-                                                          height: 50,
-                                                          decoration:
-                                                              BoxDecoration(
-                                                            border: Border.all(
-                                                              width: 3,
-                                                              color: Color
-                                                                  .fromARGB(
-                                                                      255,
-                                                                      253,
-                                                                      242,
-                                                                      145),
-                                                            ),
-                                                          ),
-                                                        ),
-                                                      ),
-                                                    ],
-                                                  ),
-                                                )),
-                                            Flexible(
-                                              flex: 100,
-                                              child: Container(),
-                                            )
-                                          ],
-                                        )),
-                                    Flexible(flex: 130, child: Container())
-                                  ],
-                                )
-                              ],
-                            ),
-                          )),
-
-                      //돌리기 버튼 부분
-                      //3D 로 제작예정
-                      Flexible(
-                          flex: 1,
-                          child: Center(
-                            child: Container(
-                                child: SolidButtonBuilder(
-                              hegiht: 80,
-                              width: 100,
-                              text: 'START',
-                              shadowcolor: Color.fromARGB(255, 114, 14, 47),
-                              color: Colors.pink,
-                              onPressed: () {
-                                foodSlotController.start();
-                              },
-                            )),
-                          ))
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: CustomColor.background,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              icon: Image.asset('assets/btn_back.png',
+                  color: Color.fromARGB(255, 0, 0, 0), width: 15),
+              onPressed: () => Get.off(() => FoodCategory()),
+            ),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Column(
+                children: [
+                  RichText(
+                    text: TextSpan(children: const <TextSpan>[
+                      TextSpan(
+                          text: ' RANDOM ',
+                          style: TextStyle(
+                              backgroundColor: CustomColor.primary,
+                              fontSize: 18,
+                              color: Colors.white)),
+                      TextSpan(text: ' '),
+                      TextSpan(
+                          text: '음식',
+                          style: TextStyle(color: Colors.black, fontSize: 18))
                     ]),
                   ),
-                ),
-              )
-            ],
+                  SizedBox(height: 15),
+                  Expanded(
+                    child: Container(
+                      width: double.infinity,
+                      child: Card(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                          side: BorderSide(
+                            color: Colors.grey.withOpacity(0.3),
+                            width: 1,
+                          ),
+                        ),
+                        child: Column(children: [
+                          Flexible(
+                              flex: 3,
+                              child: Container(
+                                height: double.infinity,
+                                color: Colors.black,
+                                child: Stack(
+                                  children: [
+                                    Container(
+                                      height: double.infinity,
+                                      color: Colors.white,
+                                      child: Image(
+                                          fit: BoxFit.fill,
+                                          image: AssetImage(
+                                              'assets/img_slot_modified.png')),
+                                    ),
+                                    Column(
+                                      children: [
+                                        Flexible(flex: 150, child: Container()),
+                                        Flexible(
+                                            flex: 130,
+                                            child: Row(
+                                              children: [
+                                                Flexible(
+                                                    flex: 110,
+                                                    child: Container()),
+                                                Flexible(
+                                                    flex: 300,
+                                                    //여기에 룰렛 삽입
+                                                    child: Container(
+                                                      decoration: BoxDecoration(
+                                                          border: Border.all(
+                                                              width: 8,
+                                                              color: Colors
+                                                                  .black
+                                                                  .withOpacity(
+                                                                      0.4)),
+                                                          borderRadius:
+                                                              BorderRadius.all(
+                                                                  Radius
+                                                                      .circular(
+                                                                          10))),
+                                                      child: Stack(
+                                                        children: [
+                                                          Container(
+                                                            child: FoodSlotMachine(
+                                                                controller:
+                                                                    foodSlotController),
+                                                          ),
+                                                          Center(
+                                                            child: Container(
+                                                              width: double
+                                                                  .infinity,
+                                                              height: 50,
+                                                              decoration:
+                                                                  BoxDecoration(
+                                                                border:
+                                                                    Border.all(
+                                                                  width: 3,
+                                                                  color: Color
+                                                                      .fromARGB(
+                                                                          255,
+                                                                          253,
+                                                                          242,
+                                                                          145),
+                                                                ),
+                                                              ),
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    )),
+                                                Flexible(
+                                                  flex: 100,
+                                                  child: Container(),
+                                                )
+                                              ],
+                                            )),
+                                        Flexible(flex: 130, child: Container())
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              )),
+
+                          //돌리기 버튼 부분
+                          //3D 로 제작예정
+                          Flexible(
+                              flex: 1,
+                              child: Container(
+                                width: double.infinity,
+                                height: double.infinity,
+                                child: Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Container(
+                                        child: SolidButtonBuilder(
+                                      hegiht: 80,
+                                      width: 100,
+                                      text: 'START',
+                                      shadowcolor:
+                                          Color.fromARGB(255, 114, 14, 47),
+                                      color: Colors.pink,
+                                      onPressed: () {
+                                        foodSlotController
+                                            .start(_confettiController);
+                                      },
+                                    )),
+                                    Positioned(
+                                      right: Get.width * 0.1,
+                                      child: Container(
+                                          child: SolidButtonBuilder(
+                                        hegiht: 50,
+                                        width: 70,
+                                        text: 'SKIP',
+                                        textstyle: TextStyle(
+                                            fontSize: 10, color: Colors.white),
+                                        shadowcolor: Colors.green,
+                                        color: Colors.greenAccent,
+                                        onPressed: () {
+                                          foodSlotController
+                                              .skip(_confettiController);
+                                        },
+                                      )),
+                                    )
+                                  ],
+                                ),
+                              ))
+                        ]),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
-      ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: ConfettiWidget(
+            confettiController: _confettiController,
+            blastDirection: pi, // radial value - LEFT
+            particleDrag: 0.05, // apply drag to the confetti
+            emissionFrequency: 0.02, // how often it should emit
+            numberOfParticles: 20, // number of particles to emit
+            gravity: 0.05, // gravity - or fall speed
+            shouldLoop: false,
+            colors: const [
+              Colors.green,
+              Colors.blue,
+              Colors.pink
+            ], // manually specify the colors to be used
+            strokeWidth: 1,
+            strokeColor: Colors.white,
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: ConfettiWidget(
+            confettiController: _confettiController,
+            blastDirection: 0, // radial value - LEFT
+            particleDrag: 0.05, // apply drag to the confetti
+            emissionFrequency: 0.02, // how often it should emit
+            numberOfParticles: 10, // number of particles to emit
+            gravity: 0.05, // gravity - or fall speed
+            shouldLoop: false,
+            colors: const [
+              Colors.green,
+              Colors.blue,
+              Colors.pink
+            ], // manually specify the colors to be used
+            strokeWidth: 1,
+            strokeColor: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/view/food/food_slot_controller.dart
+++ lib/view/food/food_slot_controller.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
@@ -29,7 +30,7 @@ class FoodSlotController implements Disposable {
 
   // 구동부분
   // 시작
-  void start() {
+  void start(ConfettiController confettiController) {
     // 랜덤한 시간을 가지고 돌리기 시작
     // 5~8초까지의 랜덤한 시간동안 돌아가게 함
     _controller.jumpToItem(0);
@@ -43,36 +44,85 @@ class FoodSlotController implements Disposable {
 
     // 여러번의 START 버튼을 눌렀을 경우 기존에 것을 취소하기 위한 구문
     _timer?.cancel();
-    openDialog();
-    _timer = Timer(Duration(seconds: randomDuration), () {});
+    confettiController.stop();
+
+    _timer = Timer(Duration(seconds: randomDuration), () {
+      openDialog();
+      confettiController.play();
+    });
   }
 
   // 구동부분
   // 스킵
-  void skip() {}
+  void skip(ConfettiController confettiController) {
+    bool timerIsActive = _timer?.isActive ?? false;
+    if (timerIsActive) {
+      _timer?.cancel();
+      openDialog();
+      confettiController.play();
+      _controller.jumpToItem(selectedIndex);
+    }
+  }
 
   void openDialog() {
     Get.dialog(
       Dialog(
+        backgroundColor: Colors.transparent,
         child: Container(
             width: Get.width * 0.8,
             height: Get.height * 0.35,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
             child: Center(
-              child: Container(
-                width: Get.width * 0.7,
-                height: Get.height * 0.3,
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.grey.withOpacity(0.3), width: 1)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      _data[selectedIndex],
-                    )
-                  ],
-                ),
+              child: Column(
+                children: [
+                  Flexible(
+                    flex: 4,
+                    child: Center(
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                        child: Container(
+                          width: Get.width * 0.7,
+                          height: Get.height * 0.3,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.grey.withOpacity(0.3),
+                                  width: 1)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                _data[selectedIndex],
+                                style: TextStyle(fontSize: 30),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                      flex: 1,
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {}, child: Text('추천 음식점')),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            ElevatedButton(
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: Text('확인'))
+                          ],
+                        ),
+                      ))
+                ],
               ),
             )),
       ),
